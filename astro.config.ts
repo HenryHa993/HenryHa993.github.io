9d2fff61b87a3d1af6bae3fa37ef04ec7429608b
Henry Ha <henryha972@gmail.com>
2025-03-07 17:49:24 +0000

    Typos

--- astro.config.ts
+++ astro.config.ts
@@ -10,7 +10,7 @@ import { remarkReadingTime } from "./src/utils/remark-reading-time";
 // https://astro.build/config
 export default defineConfig({
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://ddha.dev",
+	site: "https://dhha.dev",
 	//base: "/HenryHa993.github.io",
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
